8fd5aeb3a8b8963d7e37e2b7f278a8719236574e
Даниил <145453928+Daniil20034548@users.noreply.github.com>
2023-11-16 11:26:23 +0300

    5.3 Заполнение массива

--- src/main/java/ru/array/ArrayDefinition.java
+++ src/main/java/ru/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Ivan";
+        names[1] = "Alex";
+        names[2] = "Roma";
+        names[3] = "Daniil";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
