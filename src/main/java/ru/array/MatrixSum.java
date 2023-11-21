405fc39f898305b528eba52cbacfc8cb0f6b8ab6
Даниил <145453928+Daniil20034548@users.noreply.github.com>
2023-11-21 11:59:34 +0300

    5.24 Двухмерный массив. Циклы

--- src/main/java/ru/array/MatrixSum.java
+++ src/main/java/ru/array/MatrixSum.java
@@ -5,7 +5,6 @@ public class MatrixSum {
         int rsl = 0;
         for (int row = 0; row < array.length; row++) {
             for (int cell = 0; cell < array[row].length; cell++) {
-                int val = array[row][cell];
                 rsl = rsl + array[row][cell];
             }
         }
