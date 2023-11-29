ead49c5e24cd95e63e8d42a22cb4822c45e392c0
Даниил <145453928+Daniil20034548@users.noreply.github.com>
2023-11-29 15:56:58 +0300

    7.13 Рефакторинг Расстояние между точками

--- src/test/java/ru/condition/PointTest.java
+++ src/test/java/ru/condition/PointTest.java
@@ -8,44 +8,27 @@ class PointTest {
     @Test
     void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        assertThat(out).isEqualTo(expected, withPrecision(0.01));
-    }
-
-    @Test
-    void when22to42then2() {
-        double expected = 2;
-        int x1 = 2;
-        int y1 = 2;
-        int x2 = 4;
-        int y2 = 2;
-        double out = Point.distance(x1, y1, x2, y2);
-        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        assertThat(dist).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when31to31then0() {
-        double expected = 0;
-        int x1 = 3;
-        int y1 = 1;
-        int x2 = 3;
-        int y2 = 1;
-        double out = Point.distance(x1, y1, x2, y2);
-        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+        double expected = 2;
+        Point a = new Point(2, 2);
+        Point b = new Point(4, 2);
+        double dist = a.distance(b);
+        assertThat(dist).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when51to21then3() {
-        double expecte = 3;
-        int x1 = 5;
-        int y1 = 1;
-        int x2 = 2;
-        int y2 = 1;
-        double out = Point.distance(x1, y1, x2, y2);
-        assertThat(out).isEqualTo(expecte, withPrecision(0.01));
+        double expected = 3;
+        Point a = new Point(5, 1);
+        Point b = new Point(2, 1);
+        double dist = a.distance(b);
+        assertThat(dist).isEqualTo(expected, withPrecision(0.01));
     }
 }
