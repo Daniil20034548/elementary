56278052ad20b3c6f307ada7bd61a06561c1c573
Даниил <145453928+Daniil20034548@users.noreply.github.com>
2023-11-30 09:15:09 +0300

    7.16 Расстояни между точками в трехмерном пространстве

--- src/main/java/ru/condition/Point.java
+++ src/main/java/ru/condition/Point.java
@@ -16,14 +16,18 @@ public class Point {
         this.y = second;
     }
 
-    public  double distance(Point that) {
-        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
-    }
-
     public Point(int x, int y, int z) {
         this.x = x;
         this.y = y;
         this.z = z;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public void info() {
+        System.out.println("Point[" + this.x + ", " + this.y + "]");
     }
 
     public double distance3d(Point that) {
