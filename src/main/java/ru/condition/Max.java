40cc4ac6eac258d97e347dfccd548df55d25df45
Даниил <145453928+Daniil20034548@users.noreply.github.com>
2023-11-30 09:11:22 +0300

    7.15 Перегрузить метод max для трех чисел

--- src/main/java/ru/condition/Max.java
+++ src/main/java/ru/condition/Max.java
@@ -1,21 +1,16 @@
 package ru.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        int result = left > right ? left : right;
-        return result;
+    public static int max(int first, int second) {
+        return first > second ? first : second;
     }
 
-    public static int max(int left, int right, int first) {
-        int result = max(left, right);
-        result = first > result ? first : right;
-        return result;
+    public static int max(int first, int second, int three) {
+        return three > max(first, second) ? three : second;
     }
 
-    public static int max(int left, int right, int first, int second) {
-        int result = max(left, right, first);
-        result = second > result ? second : right;
-        return result;
+    public static int max(int first, int second, int three, int four) {
+        return four > max(first, second, three) ? four : first;
     }
 
     public static void main(String[] args) {
